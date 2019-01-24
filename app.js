c9ac48a7d160410e92fc66bdafaf2e85d893d498
apanagio <apanagio@gmail.com>
2019-01-24 10:17:24 +0200

    merged

--- app.js
+++ app.js
@@ -8,13 +8,7 @@ const port = 3000;
 
 app.get('/api', (req, res) => res.send('Hello World!'));
 
-<<<<<<< HEAD
-app.use(express.static(path.join(__dirname, 'public')))
-app.get('/', (req, res) => res.send('nothing here'))
-
-=======
 app.use(express.static(path.join(__dirname, 'public')));
->>>>>>> c5ee4b6df73aa7bb766fdfb3d4932dc05a805579
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
