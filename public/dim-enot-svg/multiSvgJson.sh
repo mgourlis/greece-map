7935394368620622059266fab3e98bf83f1d5edc
apanagio <apanagio@gmail.com>
2019-03-18 13:13:47 +0200

    added json dim enotites

--- public/dim-enot-svg/multiSvgJson.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in $(ls $1/*.svg)
-do 
-    echo $file
-    no_ext=$(basename $file .svg)
-    echo $no_ext
-    cat $file | node `dirname $0`/svgJson.js > $no_ext.json
-done
